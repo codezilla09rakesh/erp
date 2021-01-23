0ce1ea73246533bc64c1a41fe53a51a4d31181e5
Rakesh <codezilla.rakesh@gmail.com>
2021-01-23 11:16:30 +0530

    first commit

--- erp/settings.py
+++ erp/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'erp',
         'USER': 'root',
-        "PASSWORD": 123456,
+        "PASSWORD": 123456  ,
         'HOST': 'localhost',
         'PORT': '5432',
     }
